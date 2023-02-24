a252d78c5e8d982ab74f015c36f84ca2cd6ee628
Maikai1er <kolesnichenko.misha12@gmail.com>
2023-02-24 17:39:58 +0100

    add 2 arguments

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -5,7 +5,9 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
-  .name('hueta')
+  .name('gendiff')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1', '-V, --version', 'output the version number')
   .option('-f, --format <type>', 'output format')
