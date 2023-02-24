a9e687a537241d6edb39892918cbfe0d60850c11
Maikai1er <kolesnichenko.misha12@gmail.com>
2023-02-24 17:34:58 +0100

    add output type format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -8,5 +8,6 @@ program
   .name('hueta')
   .description('Compares two configuration files and shows a difference.')
   .version('0.1', '-V, --version', 'output the version number')
+  .option('-f, --format <type>', 'output format')
 
 program.parse();
