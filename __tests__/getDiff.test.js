21d1ff6dac749be7c4cdb11d8ee388d0872714ec
Maikai1er <kolesnichenko.misha12@gmail.com>
2023-03-04 00:42:41 +0100

    made test for getDiff & added commands.txt

---
+++ __tests__/getDiff.test.js
@@ -0,0 +1,6 @@
+import getDiff from '../src/getDiff.js';
+
+test('getDiff', () => {
+  const expectedResult = '{ \n  - follow: false\n    host: hexlet.io\n  - proxy: 123.234.53.22\n  - timeout: 50\n  + timeout: 20\n  + verbose: true\n}';
+  expect(getDiff('__fixtures__/file1.json', '__fixtures__/file2.json')).toEqual(expectedResult);
+});
