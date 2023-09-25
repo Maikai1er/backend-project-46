0193a7171e4e609f59a5bb9aea85538dcd2cf768
Maikai1er <kolesnichenko.misha12@gmail.com>
2023-09-25 16:50:39 +0200

    trying to get rid of let, loops and push+pop

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -1,43 +1,44 @@
 import _ from 'lodash';
 
 const plain = (obj) => {
-  const resultConstructor = [];
-  const keyConstructor = [];
   const constructString = (key, type, value1, value2) => {
-    if (type === 'added') return `Property '${keyConstructor.join('.')}' was added with value: ${value1}`;
-    if (type === 'removed') return `Property '${keyConstructor.join('.')}' was removed`;
-    return `Property '${keyConstructor.join('.')}' was updated. From ${value1} to ${value2}`;
+    if (type === 'added') return `Property '${key}' was added with value: ${value1}`;
+    if (type === 'removed') return `Property '${key}' was removed`;
+    return `Property '${key}' was updated. From ${value1} to ${value2}`;
   };
+
   const checkType = (temp) => {
     if (typeof temp === 'string') return `'${temp}'`;
     if (_.isObject(temp)) return '[complex value]';
     return temp;
   };
-  const constructLeafs = (children) => {
-    for (let i = 0; i < children.length; i += 1) {
-      const { key, type } = children[i];
-      keyConstructor.push(key);
+
+  const constructLeafs = (children, keyConstructor = []) => (
+    children.flatMap((child) => {
+      const { key, type } = child;
+      const newKeyConstructor = [...keyConstructor, key];
+
       if (type !== 'unchanged') {
         if (type === 'changed') {
-          let { oldValue, newValue } = children[i];
-          oldValue = checkType(oldValue);
-          newValue = checkType(newValue);
-          resultConstructor.push(constructString(key, type, oldValue, newValue));
-          keyConstructor.pop();
+          const { oldValue, newValue } = child;
+          const oldValueStr = checkType(oldValue);
+          const newValueStr = checkType(newValue);
+          return [constructString(newKeyConstructor.join('.'), type, oldValueStr, newValueStr)];
         } else {
-          let { value } = children[i];
-          value = checkType(value);
-          resultConstructor.push(constructString(key, type, value));
-          keyConstructor.pop();
+          const { value } = child;
+          const valueStr = checkType(value);
+          return [constructString(newKeyConstructor.join('.'), type, valueStr)];
         }
-      } else if (children[i].hasOwnProperty('children')) {
-        constructLeafs(children[i].children);
-      } if (type === 'unchanged' && !children[i].hasOwnProperty('children')) keyConstructor.pop();
-      if (i === children.length - 1) keyConstructor.pop();
-    }
-  };
-  constructLeafs(obj.children);
-  return resultConstructor.join('\n');
+      } else if (child.hasOwnProperty('children')) {
+        return constructLeafs(child.children, newKeyConstructor);
+      } else {
+        return [];
+      }
+    })
+  );
+
+  const result = constructLeafs(obj.children);
+  return result.join('\n');
 };
 
 export default plain;
